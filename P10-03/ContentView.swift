19abbad2607cff4274a35753937431129c572311
Vahtee Boo <vahtee.boo@gmail.com>
2022-01-24 16:00:07 +0200

    Normal plus Error

--- P10-03/ContentView.swift
+++ P10-03/ContentView.swift
@@ -9,12 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
-            image
-                .resizable()
-                .scaledToFit()
-        } placeholder: {
-            ProgressView()
+        VStack {
+            AsyncImage(url: URL(string: "https://cineuropa.org/imgCache/2018/12/11/1544541049322_0620x0435_0x0x0x0_1573329743140.jpg")) { image in
+                image
+                    .resizable()
+                    .scaledToFit()
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(height: 200)
+        }
+        
+        AsyncImage(url: URL(string: "https://hws.dev/img/bad/png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading the image.")
+            } else {
+                ProgressView()
+            }
         }
         .frame(width: 200, height: 200)
     }
